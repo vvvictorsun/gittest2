14d3f6279951bd2c30156d7ae13dcc12a6077997
Zheng Sun <vvvictorsun@gmail.com>
2020-12-15 17:55:37 +0000

    milestone 0

---
+++ app/src/main/java/com/example/gittest2/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.gittest2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+//milestone 0
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
